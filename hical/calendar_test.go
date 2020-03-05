a17a9dc27549df4acccbada67c5899160c61fdf5
Rolando <rolando.umana@gmail.com>
2020-03-05 11:35:51 -0700

    Implemented hical, using the sort function from go

--- hical/calendar_test.go
+++ hical/calendar_test.go
@@ -2,37 +2,6 @@ package hical
 
 import "testing"
 
-func TestSort(t *testing.T) {
-	cases := []struct {
-		in, out [][]int
-	}{
-		{
-			[][]int{{2, 3}, {8, 10}, {1, 2}, {0, 1}},
-			[][]int{{0, 1}, {1, 2}, {2, 3}, {8, 10}},
-		},
-		{
-			[][]int{{4, 6}, {10, 12}, {1, 3}},
-			[][]int{{1, 3}, {4, 6}, {10, 12}},
-		},
-	}
-
-	for _, c := range cases {
-		result := Sort(c.in)
-		length := len(result)
-		if length == 0 {
-			t.Errorf("Error on input %v, result is empty", c.in)
-		} else if length != len(c.out) {
-			t.Errorf("Different size. Input: %v, result: %v, expected: %v", c.in, result, c.out)
-		}
-		for i, meeting := range result {
-			if meeting[0] != c.out[i][0] || meeting[1] != c.out[i][1] {
-				t.Errorf("Error sorting array. Input: %v, result: %v, expected: %v", c.in, result, c.out)
-				break
-			}
-		}
-	}
-}
-
 func TestMergeRanges(t *testing.T) {
 	cases := []struct {
 		meetings, ranges [][]int
